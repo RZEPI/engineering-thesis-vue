e777477eaa9dae42d0ffcf4664545c250287ef32
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-07-26 22:07:42 +0200

    added animation for the sidebar

--- vue-app/src/components/UI/BaseSidebar.vue
+++ vue-app/src/components/UI/BaseSidebar.vue
@@ -1,26 +1,25 @@
 <template>
-  <header>
-    <div
-      class="burger-button"
-      :class="{ expanded: isExpanded }"
-      @click="toggleExpand"
-    >
+  <header :class="{'border-shadow': isExpanded}">
+    <div class="burger-button" :class="btnClass" @click="toggleExpand">
       <span></span>
       <span></span>
       <span></span>
     </div>
   </header>
-  <nav v-if="isExpanded">
-    <ul>
-      <sidebar-link
-        v-for="link in links"
-        :key="link.label"
-        :image-src="link.imageSrc"
-        :label="link.label"
-        :link="link.link"
-      ></sidebar-link>
-    </ul>
-  </nav>
+
+  <transition name="sidebar" mode="out-in">
+    <nav v-if="isExpanded">
+      <ul>
+        <sidebar-link
+          v-for="link in links"
+          :key="link.label"
+          :image-src="link.imageSrc"
+          :label="link.label"
+          :link="link.link"
+        ></sidebar-link>
+      </ul>
+    </nav>
+  </transition>
   <div v-if="isExpanded" class="backdrop" @click="toggleExpand"></div>
 </template>
 
@@ -28,9 +27,12 @@
 import SidebarLink from "./SidebarLink.vue";
 import { sites } from "../../static/sites";
 
-import { ref, toRef } from "vue";
+import { ref, toRef, computed } from "vue";
 const isExpanded = ref<boolean>(false);
 const links = toRef(sites);
+const btnClass = computed(() => {
+  return isExpanded.value ? "expanded" : "collapsed";
+});
 
 function toggleExpand() {
   isExpanded.value = !isExpanded.value;
@@ -38,10 +40,27 @@ function toggleExpand() {
 </script>
 
 <style scoped>
+.sidebar-enter-from,
+.sidebar-leave-to {
+  width: 0;
+}
+.sidebar-enter-to,
+.sidebar-leave-from {
+  width: 35%;
+}
+
+.sidebar-enter-active {
+  transition: width 0.3s ease-out;
+}
+
+.sidebar-leave-active {
+  transition: width 0.3s ease-in;
+}
+
 nav {
   background-color: rgb(65, 183, 131);
   height: 100vh;
-  width: 35%;
+  width: 35vw;
   position: fixed;
   z-index: 10;
   float: left;
@@ -52,18 +71,19 @@ ul {
   padding: 0;
 }
 header {
-  width: 35%;
+  width: 35vw;
   position: fixed;
   z-index: 10;
-  top: 0.3em;
+  top: 0;
   height: 4.5vh;
 }
 .burger-button {
-  margin-left: 3em;
+  transition: transform 0.3s ease-out;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  width: 4.5vw;
+  margin: 0.3em 0 0 2.7em;
+  width: 2.5em;
   height: 80%;
   cursor: pointer;
 }
@@ -72,8 +92,14 @@ header {
   width: 2.5em;
   height: 0.4em;
 }
-.burger-button.expanded {
-  margin-left: calc(100% - 4.5vw);
+
+.expanded {
+  transform: translateX(calc(35vw - 6em));
+  transition-delay: 0.04s;
+}
+
+.collapsed {
+  transform: translateX(0);
 }
 
 .backdrop {
@@ -82,5 +108,10 @@ header {
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 5;
+}
+
+.border-shadow{
+  transition: box-shadow 0.4s;
+  box-shadow: 4px 0 4px 3px rgba(0, 0, 0, 0.5);
 }
 </style>
