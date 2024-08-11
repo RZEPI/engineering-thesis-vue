4e84a3daf311d50bdbf2ffb9311b92df1812886b
Marcel <marziel2001@gmail.com>
2024-08-11 17:13:49 +0200

    [Vue][TablePage] Add delete top item, add delete every nth item, format code

--- vue-app/src/pages/TablePage.vue
+++ vue-app/src/pages/TablePage.vue
@@ -1,52 +1,71 @@
 <template>
-    <div>
-        <button @click="addRecord">Add</button>
-        <table>
-            <thead>
-                <th></th>
-                <th></th>
-                <th></th>
-            </thead>
-            <tr :key=tuple[0] v-for="tuple in tuples">
-                <td>{{tuple[0]}}</td>
-                <td>{{tuple[1]}}</td>
-                <td>{{tuple[2]}}</td>
-            </tr>
-        </table>
-    </div>
+  <div>
+    <button @click="addRecord">Add</button>
+    <button @click="deleteRecord">Delete</button>
+    <button @click="deleteEveryNthRecord(2)">Delete Nth</button>
+    <button @click="printLog">Log</button>
+    <table>
+      <thead>
+        <th></th>
+        <th></th>
+        <th></th>
+      </thead>
+      <tr :key="tuple" v-for="tuple in tuples">
+        <!-- Todo: change row to component, where each of them has a state and edit mode can be set on it -->
+        <button>Edit</button>
+        <td><input type="number" v-model="tuple[0]" /></td>
+        <td>{{ tuple[1] }}</td>
+        <td>{{ tuple[2] }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import { NamesTable } from '../static/RandomDataTables';
-import { NumberTable } from '../static/RandomDataTables';
-import {onBeforeMount} from 'vue';
+import { onBeforeMount, ref } from "vue";
+import { NamesTable } from "../static/RandomDataTables";
+import { NumberTable } from "../static/RandomDataTables";
 
-const tuples = ref<[number,string,number][]>([])
+const tuples = ref<[number, string, number][]>([]);
 
 let key: number = 0;
 
 onBeforeMount(generateArray);
 
-function addRecord()
-{
-    const NameIndex: number =  Math.floor(Math.random() * NamesTable.length);
-    const LevelIndex: number = Math.floor(Math.random() * NumberTable.length);
-    tuples.value.unshift([key++, NamesTable[NameIndex], NumberTable[LevelIndex]]);
+function addRecord() {
+  const NameIndex: number = Math.floor(Math.random() * NamesTable.length);
+  const LevelIndex: number = Math.floor(Math.random() * NumberTable.length);
+  tuples.value.unshift([key++, NamesTable[NameIndex], NumberTable[LevelIndex]]);
 }
 
-function generateArray()
-{
-    const generatedArray: [number, string, number][] = [];
+function deleteRecord() {
+  tuples.value.splice(0, 1);
+}
 
-        for(let i = 0; i<5; i++)
-        {
-            const NameIndex: number =  Math.floor(Math.random() * NamesTable.length);
-            const LevelIndex: number = Math.floor(Math.random() * NumberTable.length);
-            generatedArray.unshift([key++, NamesTable[NameIndex], NumberTable[LevelIndex]]);
-        }
+function deleteEveryNthRecord(n: number) {
+  for (let i = 0; i < tuples.value.length; i += n) {
+    tuples.value.splice(i--, 1);
+  }
+}
 
-        tuples.value.unshift(...generatedArray);
+function generateArray() {
+  const generatedArray: [number, string, number][] = [];
+
+  for (let i = 0; i < 20; i++) {
+    const NameIndex: number = Math.floor(Math.random() * NamesTable.length);
+    const LevelIndex: number = Math.floor(Math.random() * NumberTable.length);
+    generatedArray.unshift([
+      key++,
+      NamesTable[NameIndex],
+      NumberTable[LevelIndex],
+    ]);
+  }
+
+  tuples.value.unshift(...generatedArray);
+}
+
+function printLog() {
+  console.log(tuples.value);
 }
 </script>
 
