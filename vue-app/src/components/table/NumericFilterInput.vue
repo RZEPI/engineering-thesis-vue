f5c3cedd348e19681f246adffb8c429380fe06d2
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-11-03 13:33:50 +0100

    added UI version of filter

---
+++ vue-app/src/components/table/NumericFilterInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <li class="numeric-input">
+        <label :for="className">{{label}}</label>
+        <input type="number" :class="className" :id="className" :value="value" @change="handleNumberInputChange" />
+    </li>
+</template>
+
+<script setup lang="ts">
+import { NumericFilterInputProps } from '../../models/table/NumericFilterInputProps';
+
+const {className, value, handleChange} = defineProps<NumericFilterInputProps>();
+function handleNumberInputChange(e: Event)
+{
+    handleChange(e)
+}
+
+const label = `${className.charAt(0).toUpperCase()}${className.substring(1)}:`;
+</script>
+
+<style scoped>
+.numeric-input{
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+}
+.numeric-input input{
+    width: 60%;
+    font-size: 1.5em;
+    padding: 0.3em;
+    border-radius: 0.4em;
+}
+
+label {
+    font-size: 1.5em;
+}
+</style>
---
+++ vue-app/src/components/table/NumericFilterTab.vue
@@ -0,0 +1,53 @@
+<template>
+        <li class="header">{{ headerLabel }}</li>
+          <numeric-filter-input
+            className="min"
+            :value="currentFilter.min"
+            :handleChange="handleNumericInputChange"
+            :key="`min_${activeTab}`"
+          ></numeric-filter-input>
+          <numeric-filter-input
+            className="max"
+            :value="currentFilter.max"
+            :handleChange="handleNumericInputChange"
+            :key="`max_${activeTab}`"
+          ></numeric-filter-input>
+          <li :key="`interval_${activeTab}`">
+            <label for="open-interval">Open interval</label>
+            <input
+              type="checkbox"
+              class="open-interval"
+              id="open-interval"
+              :checked="currentFilter.isOpen"
+              @change="handleIsOpenChange"
+            />
+          </li>
+</template>
+
+<script setup lang="ts">
+import NumericFilterInput from './NumericFilterInput.vue';
+import { NumericFilterTabProps } from '../../models/table/NumericFilterTabProps';
+const {currentFilter, activeTab, handleNumericInputChange, handleIsOpenChange} = defineProps<NumericFilterTabProps>();
+
+const headerLabel = activeTab.charAt(0).toUpperCase() + activeTab.slice(1) + " range";
+</script>
+<style scoped>
+
+label {
+    font-size: 1.5em;
+}
+
+input[type="checkbox"] {
+    width: 1.5em;
+    height: 1.5em;
+}
+.header {
+    font-size: 1.7em;
+    margin-top: -1em;
+    text-align: center;
+}
+
+.open-interval {
+    margin-left: 4em;
+}
+</style>
