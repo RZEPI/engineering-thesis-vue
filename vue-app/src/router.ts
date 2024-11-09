37de92048e38e69b3685f71c1a6d5db9b626d521
Marcel Zielinski <marziel2001@gmail.com>
2024-11-09 17:02:03 +0100

    [Vue][GridPage] Add GridPage to router and sidebar

--- vue-app/src/router.ts
+++ vue-app/src/router.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
 import TablePage from "./pages/TablePage.vue";
 import NotFound from "./pages/NotFound.vue";
+import GridPage from "./pages/GridPage.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: HomePage },
     { path: "/Table", component: TablePage },
+    { path: "/Grid", component: GridPage },
     { path: "/:notFound(.*)", component: NotFound },
   ],
   linkActiveClass: "active",
