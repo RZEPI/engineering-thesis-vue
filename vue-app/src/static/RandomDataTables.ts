68b16c9120942abcaebbc38834dc4a35b5c7a7ea
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-11-17 17:20:11 +0100

    added button for clearing filter

--- vue-app/src/static/RandomDataTables.ts
+++ vue-app/src/static/RandomDataTables.ts
@@ -1,3 +1,6 @@
+import { StringFilter, TableFilter } from "../models/table/TableFilter";
+import { TableRowData } from "../models/table/TableRowData";
+
 export const numberTable: number[] = [
   21, 32, 43, 54, 65, 76, 87, 89, 109, 110, 211, 312, 413, 514, 615,
 ];
@@ -11,3 +14,18 @@ export const namesTable: string[] = [
   "Krzysiek",
   "Ola",
 ];
+
+export function makeDefaultFilter():TableFilter {
+  const nameFilters: StringFilter[] =  namesTable.map((name) => ({ value: name, isChecked: true }));
+  return {
+    id: { min: undefined, max: undefined, isOpen: false },
+    name: nameFilters,
+    level: { min: undefined, max: undefined, isOpen: false },
+  };
+};
+
+export const tableDummyRow: TableRowData = {
+  id: 0,
+  name: "Name",
+  level: 0,
+};
