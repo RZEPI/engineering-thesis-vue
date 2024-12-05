566f486fa1bc6ccfdac0452d4e3745ebafc6e8fa
Łukasz Prahl <102081415+NOTSAHLO@users.noreply.github.com>
2024-12-05 12:58:08 +0100

    fixed a bug

--- vue-app/vite.config.ts
+++ vue-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
