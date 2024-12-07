70194dcd7ce45628e24d084e235fb06678c7f38e
Marcel Zielinski <marziel2001@gmail.com>
2024-12-07 15:25:31 +0100

    [Vue][GridPage] Add CSS parser to codelisting

--- vue-app/src/components/CodeListing.vue
+++ vue-app/src/components/CodeListing.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="code-listing__container">
     &#123;
-    <div
-        v-for="(propertyKey, propertyValue) in cssProps"
-        :key="propertyKey">
-        <span class="property">&emsp;&emsp;{{ propertyValue }}:</span>
-        <span class="value">&nbsp;&nbsp;{{ propertyKey }}</span>;
+    <div v-for="entry in parsedEntries" :key="entry.propertyKey">
+      <span class="property">&emsp;&emsp;{{ entry.propertyKey }}:</span>
+      <span class="value">&nbsp;&nbsp;{{ entry.propertyValue }}</span
+      >;
     </div>
     &#125;
   </div>
 </template>
 
 <script setup lang="ts">
-import { CSSProperties } from "vue";
-
-defineProps<{ cssProps: CSSProperties }>();
+import { computed, CSSProperties } from "vue";
+import { parseCssEntries } from "../util/utils";
+const props = defineProps<{ cssProps: CSSProperties }>();
+const parsedEntries = computed<
+  { propertyKey: string; propertyValue: string }[]
+>(() => parseCssEntries(props.cssProps as CSSProperties));
 </script>
 
 <style lang="css" scoped></style>
