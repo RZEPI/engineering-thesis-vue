7abe405c0b9a8a30410c3324b18d20fb1652f6eb
Łukasz Prahl <102081415+NOTSAHLO@users.noreply.github.com>
2024-12-09 12:12:07 +0100

    Made desc more consistent

--- vue-app/src/static/sites.ts
+++ vue-app/src/static/sites.ts
@@ -22,11 +22,13 @@ export const sites: Array<Site> = [
   {
     label: "Table",
     link: "/table",
-    description: "Table page, page responsible for testing rendering of big amounts of data. It also displays filtering modal.",
+    description: 
+      "Table page, page responsible for testing rendering of big amounts of data. It also displays filtering modal.",
   },
   {
     label: "RecRndr",
     link: "/recursive-rendering",
-    description: "Page responsible for testing recursive rendering of components.",
+    description: 
+      "Page responsible for testing recursive rendering of components.",
   },
 ];
