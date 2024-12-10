b99f483cb28533b384535b1ebbf46eb244249793
Łukasz Prahl <102081415+NOTSAHLO@users.noreply.github.com>
2024-12-10 11:02:20 +0100

    First batch of tests

---
+++ vue-app/test/components/UI/BaseSidebar.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import BaseSidebar from "../../../src/components/UI/BaseSidebar.vue";
+
+describe("BaseSidebar.vue", () =>{
+    it("Should render collapsed", () => {
+        const wrapper = shallowMount(BaseSidebar);
+
+        expect(wrapper.find('nav').exists()).toBe(false);
+    });
+    it("Should render Navbar after pressing the button", () => {
+        const wrapper = shallowMount(BaseSidebar);
+
+        wrapper.find('.burger-button').trigger('click');
+
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.find('nav').exists()).toBe(true);
+        });
+    });
+});
---
+++ vue-app/test/components/recursive/RecursiveComponent.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import RecursiveComponent from "../../../src/components/recursive/RecursiveComponent.vue";
+import { VueWrapper } from "@vue/test-utils";
+
+describe("RecursiveComponent.vue", () => {
+    it("Renders 4 instances of Recursive Component", () => {
+        const wrapper = mount(RecursiveComponent, {
+            props:{
+                message: "",
+                depth: 0,
+                maxDepth: 4,
+            },
+        });
+        const instances: VueWrapper[] = wrapper.findAllComponents(RecursiveComponent);
+        expect(instances.length).toBe(4);
+    })
+});
